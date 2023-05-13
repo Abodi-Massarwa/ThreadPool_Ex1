319f41a3246b48c173a63171233f27efdd92bc98
BlackEswadeh <abodi.msarwi@gmail.com>
2023-05-13 18:06:39 +0300

    fixed problem without the need for mutex & conds :)

--- stdin_main.c
+++ stdin_main.c
@@ -14,6 +14,7 @@ struct my_thread{
     int m_start_index; /// since we are dealing with char* tasks can be divided by simply indexes
     int m_end_index;
     int m_is_active; /// 1 active 0 not active
+    char m_string[(MAX_SIZE/LIST_SIZE)+1]; /// worst case scenario each thread holds 1024/6 chars
     pthread_t m_thread;
 };
 
@@ -46,20 +47,27 @@ void* thread_encrypt_function(void* thread)
      *  and to not use mutex which is gonna be costy
      *  TODO 1) lets give each thread its own char[]
      */
+    my_thread* current_thread_ptr=((my_thread*)thread);
     my_thread current_thread= *((my_thread*) thread);
     print_mythread_info(current_thread);
     size_t length= current_thread.m_end_index-current_thread.m_start_index+1;
     char current_sub_string[length];
+    current_sub_string[length]='\0';
     /// now lets get to real work and call the desired function
     strncpy(current_sub_string, our_string + current_thread.m_start_index, length);
     current_sub_string[length] = '\0';  // Add null terminator
-    //printf("\nTHREAD[%d] substring before encryption= %s",current_thread.m_id,current_sub_string);
+    printf("\nTHREAD[%d] substring before encryption= %s",current_thread.m_id,current_sub_string);
     encrypt(current_sub_string,our_key);
     //printf("\nTHREAD[%d] substring after encryption= %s",current_thread.m_id,current_sub_string);
     /// now append this to the global result string
-    pthread_mutex_lock(&our_mutex);
-    strcat(result_string, current_sub_string);
-    pthread_mutex_unlock(&our_mutex);
+//    pthread_mutex_lock(&our_mutex);
+//    strcat(result_string, current_sub_string);
+//    pthread_mutex_unlock(&our_mutex);
+    ((my_thread*)thread)->m_string[(MAX_SIZE/LIST_SIZE)]='\0';
+    strcat(current_thread_ptr->m_string, current_sub_string); // copies each encrypted substring to the thread field
+    /// works but somehow the m_string is empty outside the function
+    //printf("thread[%d]->m_sring=%s\n",current_thread.m_id,((my_thread*)thread)->m_string);
+
 
 
 
@@ -80,6 +88,7 @@ void start_multithreading(char indicator, int key, char* data, int char_count)
 {
     /// lets divide tasks for each thread based on how many chars we have
     char_count= strlen(data);
+    printf("\nchar counter is : %d\n",char_count);
     int remainder=0;
     int chunk_size = char_count / LIST_SIZE;
     int current_char_number=char_count;
@@ -228,54 +237,64 @@ int main(int argc, char *argv[])
 	 * after we exit the loop we need to make sure whether the text reached the maximum length
 	 * or simply because the file ended and the counter isn't 1024
 	 */
-	if (counter > 0)
-	{
+	if (counter > 0) {
 
-		char lastData[counter];
-		lastData[0] = '\0';
-		strncat(lastData, data, counter);
-        lastData[counter-1]='\0';
+        char lastData[counter];
+        lastData[0] = '\0';
+        strncat(lastData, data, counter);
+        lastData[counter - 1] = '\0';
         //printf("lastdata[counter]=%c",lastData[counter]);
-        char* action = argv[2];
-        strcpy(our_string,lastData);
-        our_key=key;
-        if(action[1]=='e') {
-            printf("\nour string is :\n%s\n",lastData);
+        char *action = argv[2];
+        strcpy(our_string, lastData);
+        our_key = key;
+        if (action[1] == 'e') {
+            printf("\nour string is :\n%s\n", lastData);
             // TODO here we need to call pthread_t_create but before we need to divide the job on threads
-            start_multithreading('e',key,lastData,counter);
+            start_multithreading('e', key, lastData, counter);
             encrypt(lastData, key);
-            printf("Encrypted data:\n%s\n",lastData);
+            printf("Encrypted data:\n%s\n", lastData);
 //            decrypt(lastData,key);
 //            printf("Decrypted data:\n%s\n",lastData);
-        }
-        else {// TODO "-d" as for Decrypt
-            printf("\nour string is :\n%s\n",lastData);
+        } else {// TODO "-d" as for Decrypt
+            printf("\nour string is :\n%s\n", lastData);
             // TODO here we need to call pthread_t_create but before we need to divide the job on threads
-            start_multithreading('d',key,lastData,counter);
+            start_multithreading('d', key, lastData, counter);
             decrypt(lastData, key);
-            printf("Decrypted data:\n%s\n",lastData);
+            printf("Decrypted data:\n%s\n", lastData);
         }
-        data[counter]='\0'; // TODO to make sure the string is printed without extra garbage letters
+        data[counter] = '\0'; // TODO to make sure the string is printed without extra garbage letters
 //        printf("original data:\n %s\n",data);
 //		printf("Encrypted data:\n %s\n",lastData);
 
-        printf("\nTHE GLOBAL RESULT STRING IS %s\n",result_string);
-        if(strcmp(result_string,lastData)==0)
+//        printf("\nTHE GLOBAL RESULT STRING IS %s\n",result_string);
+//        if(strcmp(result_string,lastData)==0)
+//            printf("STRINGS EQUAL TO EXPECTED\n");
+//            else
+//        {
+//            printf("STRINGS NOT EQUAL\n");
+//        }
+
+        /*
+         * join threads
+         */
+        for (int i = 0; i < LIST_SIZE; ++i) {
+            if (thread_list[i].m_is_active == 1)
+                if (pthread_join(thread_list[i].m_thread, NULL) != 0)
+                    printf("ERROR JOINING THREAD [%d]", i);
+        }
+        for (int i = 0; i < LIST_SIZE; ++i) {
+            strcat(result_string,thread_list[i].m_string);
+            printf(" string of thread[%d] =%s",i,thread_list[i].m_string);
+
+        }
+        printf("\nTHE GLOBAL RESULT STRING IS %s\n", result_string);
+        printf("\nTHE lastdata STRING IS %s\n", lastData);
+        if (strcmp(result_string, lastData) == 0)
             printf("STRINGS EQUAL TO EXPECTED\n");
-            else
-        {
+        else {
             printf("STRINGS NOT EQUAL\n");
         }
-	}
-    /*
-     * join threads
-     */
-    for (int i = 0; i < LIST_SIZE; ++i) {
-        if(thread_list[i].m_is_active==1)
-            if(pthread_join(thread_list[i].m_thread,NULL)!=0)
-                printf("ERROR JOINING THREAD [%d]",i);
+        pthread_mutex_destroy(&our_mutex);
     }
-    pthread_mutex_destroy(&our_mutex);
-
 	return 0;
 }
